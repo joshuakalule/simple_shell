720691eca5404a970a5b7eabde6ae833dbdc0e59
Joshua Kalule <kaljoshkalule@gmail.com>
2023-04-30 22:44:40 +0000

    add betty file

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: number of arguments passed
+ * @argv: argument list
+ * @env: environment variable list
+ *
+ * Description: Beautiful code to pass Betty
+ *
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char *argv[], char *env[])
+{
+	printf("Hello World\n");
+
+	return (0);
+}
